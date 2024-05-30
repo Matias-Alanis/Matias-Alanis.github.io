afe9e61a4df20d34f48ceff79fcd0522d734db19
Matias-Alanis <matias.alanis.job@gmail.com>
2024-05-30 16:18:01 -0300

    modify listaNav , add banner background

---
+++ js/fetch_peliculas.js
@@ -0,0 +1,12 @@
+// Definimos la URL base de la API de The Movie DB
+const API_SERVER = 'https://api.themoviedb.org/3';
+
+// Opciones para las peticiones fetch a la API
+const options = {
+    method: 'GET', // Método de la petición (GET)
+    headers: {
+        accept: 'application/json', // Tipo de respuesta esperada (JSON)
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhYTJjYTAwZDYxZWIzOTEyYjZlNzc4MDA4YWQ3ZmNjOCIsInN1YiI6IjYyODJmNmYwMTQ5NTY1MDA2NmI1NjlhYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.4MJSPDJhhpbHHJyNYBtH_uCZh4o0e3xGhZpcBIDy-Y8'
+        
+    }
+};
